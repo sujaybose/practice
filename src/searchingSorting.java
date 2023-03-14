b879b7dd1e5ef0191aa87a869ecedcb2a4cf7379
sujaybose <sujay.bose@gmail.com>
2023-03-14 08:18:21 +0530

    2nd commit

--- src/searchingSorting.java
+++ src/searchingSorting.java
@@ -1,6 +1,21 @@
 import java.util.Arrays;
 
 public class searchingSorting {
+
+    public static int[] bubbleSort(int[] arr){
+        int l= arr.length;
+        for (int i = 0; i < l/2; i++) {
+            for (int j = 0; j < l-i-1; j++) {
+                if(arr[j]>arr[j+1]){
+                    int temp= arr[j];
+                    arr[j]= arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+
+        }
+        return arr;
+    }
     public static int binarySearch(int[] array, int key){
         Arrays.sort(array);
         int first=0, last=array.length-1, mid;
@@ -28,6 +43,11 @@ public class searchingSorting {
 
     public static void main(String[] args) {
         int [] arra= {2,4,5,6,7,8,9};
-        System.out.println(binarySearch(arra, 5));
+        int [] arr2={1,3,7,2,4,8,5};
+       // System.out.println(binarySearch(arra, 5));
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println(bubbleSort(arr2)[i]);
+
+        }
     }
 }
